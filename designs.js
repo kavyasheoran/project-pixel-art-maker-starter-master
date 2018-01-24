e461e9605012f42c953a7446ca639ae287661b87
Kavya Sheroan <kavya_sheoran@yahoo.com>
2018-01-24 18:18:10 +0530

    Specifications completed

--- designs.js
+++ designs.js
@@ -1,23 +1,29 @@
+// selecting size input
+const row_input = $("#input_height");
+const col_input = $("#input_width");
+
+//when size is submitted by the user, makeGrid() is called
 $("#sizePicker").submit(function(event) {
 	$("#pixel_canvas").empty();
 	makeGrid();
 	event.preventDefault();
 });
 
+// grid generation based on user input
 function makeGrid() {
-	const width=$("#input_width").val();
-	const height=$("#input_height").val();
+	let row_count = row_input.val();
+	let col_count = col_input.val();
 
 	let table=$("#pixel_canvas");
-	for(let r=0; r<height; r++){
+	for(let r=0; r<row_count; r++){
     	let tr = $("<tr>");
-    	for(let c=0; c<width; c++)
+    	for(let c=0; c<col_count; c++)
         	tr.append("<td></td>");
     	table.append(tr);
 	}
 
-	const color=$("#colorPicker").val();
-  	$("td").click(function() {
-    	$(this).css("background",color);
-  	});
+  	$("td").click(function(){
+     	const color = $("#colorPicker").val();
+    	$(this).css("background-color", color);
+	});
 }
